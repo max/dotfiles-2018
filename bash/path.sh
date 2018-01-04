baa7e5753cbd76207e1df5d63f556d39f2a75752
Max Schoening <max@max.wtf>
2018-01-04 13:33:27 -0800

    Remove unused gcloud SDK sourcing
    gcloud is now a Docker image

--- bash/path.sh
+++ bash/path.sh
@@ -8,7 +8,3 @@ export FZF_DEFAULT_OPTS='
   --color=fg:#839496,header:#586e75,info:#cb4b16,pointer:#719e07
   --color=marker:#719e07,fg+:#839496,prompt:#719e07,hl+:#719e07
 '
-
-# TODO: Move into Docker image and alias to Docker image
-if [ -f '/Users/max/Code/google-cloud-sdk/path.bash.inc' ]; then source '/Users/max/Code/google-cloud-sdk/path.bash.inc'; fi
-if [ -f '/Users/max/Code/google-cloud-sdk/completion.bash.inc' ]; then source '/Users/max/Code/google-cloud-sdk/completion.bash.inc'; fi
