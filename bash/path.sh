6807c579748a77cd208e5c8b03e0b38e6e89db8a
Max Schoening <max@max.wtf>
2018-04-16 11:11:51 -0700

    Add base16 color setup

--- bash/path.sh
+++ bash/path.sh
@@ -2,9 +2,5 @@
 export GOPATH=~/Code/go
 export PATH=$PATH:$(go env GOPATH)/bin
 
-# Solarized colors
-export FZF_DEFAULT_OPTS='
-  --color=bg+:#073642,bg:#002b36,spinner:#719e07,hl:#586e75
-  --color=fg:#839496,header:#586e75,info:#cb4b16,pointer:#719e07
-  --color=marker:#719e07,fg+:#839496,prompt:#719e07,hl+:#719e07
-'
+# TODO: Move into Docker image and don't require locally
+export PATH="$HOME/.cargo/bin:$PATH"
--- bash/prompt.sh
+++ bash/prompt.sh
@@ -1,14 +1,8 @@
 # TODO: Remove macOS specifics
-if [ -f $(brew --prefix)/etc/bash_completion.d/git-prompt.sh ]; then
-  . $(brew --prefix)/etc/bash_completion.d/git-prompt.sh
+if [ -f `brew --prefix`/etc/bash_completion.d/git-prompt.sh ]; then
+  . `brew --prefix`/etc/bash_completion.d/git-prompt.sh
 fi
-
-RESET="\[\017\]"
-NORMAL="\[\033[0m\]"
-MUTED="\[\033[01;33m\]"
-BLUE="\[\033[0;34m\]"
-PURPLE="\[\033[0;35m\]"
 
 GIT_PS1_SHOWDIRTYSTATE=true
 GIT_PS1_SHOWUNTRACKEDFILES=true
-PS1="${RESET}${MUTED}\$${NORMAL} ${BLUE}\W${GREEN}\$(__git_ps1 \" %s\")${NORMAL} "
+PS1="Ṁ \W\e[2m\$(__git_ps1 \" %s\")\e[0m → "
